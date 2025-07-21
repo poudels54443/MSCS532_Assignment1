6ce6dd826ca5032c0303008cccfccb4f19a38fd8
poudels54443 <113079803+poudels54443@users.noreply.github.com>
2025-07-20 21:32:38 -0500

    Set up basic function structure for insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,12 @@
+# Insertion Sort in Monotonically Decreasing Order
+
+def insertion_sort_desc(arr):
+    #TODO
+    pass
+
+# Example usage
+if __name__ == "__main__":
+    data = [5, 2, 9, 1, 5, 6]
+    print("Original array:", data)
+    insertion_sort_desc(data)
+    print("Sorted array in decreasing order:", data)
